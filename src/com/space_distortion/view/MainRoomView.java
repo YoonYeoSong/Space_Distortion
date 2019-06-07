a7a0c36e401da7593a458ce96146e7f685eaf47b
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-06-07 10:31:35 +0900

    룸에 대한 뷰 위치 조정

--- src/com/space_distortion/view/MainRoomView.java
+++ src/com/space_distortion/view/MainRoomView.java
@@ -13,91 +13,87 @@ import com.space_distortion.controller.TimerThread;
 import com.space_distortion.event.SpaceActionEvent;
 import com.space_distortion.model.vo.RoomInfo;
 
-public class MainRoomView extends SpaceActionEvent implements ViewIndex{
+public class MainRoomView extends SpaceActionEvent implements ViewIndex {
 
 	public MainRoomView() {
-			
-		}
-	
-	public void initialize(SpaceController sc, JFrame mainJframe, List<RoomInfo> roomInfoList)
-	{	
+
+	}
+
+	public void initialize(SpaceController sc, JFrame mainJframe, List<RoomInfo> roomInfoList) {
 		JPanel jp = new JPanel();
-		jp.setBounds(10, 10, 1024, 768);
+		jp.setBounds(10, 10, 1008, 728);
 		jp.setLayout(null);
-				
-		
+
 		JButton btnRoom1 = new JButton("1\uBC88\uBC29");
-		btnRoom1.setBounds(14, 122, 105, 49);
-		
-		//쓰레드라벨1
+		btnRoom1.setBounds(88, 166, 105, 49);
+
+		// 쓰레드라벨1
 		JLabel lbTime1 = new JLabel("");
-		lbTime1.setBounds(47, 82, 62, 49);
+		lbTime1.setBounds(120, 122, 62, 49);
 		jp.add(lbTime1);
-		CallThread(lbTime1,roomInfoList.get(0).getRemTime(),roomInfoList,0,btnRoom1);
-		
+		CallThread(lbTime1, roomInfoList.get(0).getRemTime(), roomInfoList, 0, btnRoom1);
+
 		JButton btnRoom2 = new JButton("2\uBC88\uBC29");
-		btnRoom2.setBounds(131, 122, 105, 49);
-		
-		//쓰레드라벨2
+		btnRoom2.setBounds(327, 166, 105, 49);
+
+		// 쓰레드라벨2
 		JLabel lbTime2 = new JLabel("");
-		lbTime2.setBounds(170, 82, 105, 49);
+		lbTime2.setBounds(360, 122, 105, 49);
 		jp.add(lbTime2);
-		CallThread(lbTime2,roomInfoList.get(1).getRemTime(),roomInfoList,1,btnRoom2);
-		
+		CallThread(lbTime2, roomInfoList.get(1).getRemTime(), roomInfoList, 1, btnRoom2);
+
 		JButton btnRoom3 = new JButton("3\uBC88\uBC29");
-		btnRoom3.setBounds(250, 122, 105, 49);
-		
-		//쓰레드라벨3
+		btnRoom3.setBounds(578, 166, 105, 49);
+
+		// 쓰레드라벨3
 		JLabel lbTime3 = new JLabel("");
-		lbTime3.setBounds(300, 82, 105, 49);
+		lbTime3.setBounds(610, 122, 105, 49);
 		jp.add(lbTime3);
-		CallThread(lbTime3,roomInfoList.get(2).getRemTime(),roomInfoList,2,btnRoom3);
-	
+		CallThread(lbTime3, roomInfoList.get(2).getRemTime(), roomInfoList, 2, btnRoom3);
+
 		JButton btnRoom4 = new JButton("4\uBC88\uBC29");
-		btnRoom4.setBounds(369, 122, 105, 49);
-		//쓰레드라벨
+		btnRoom4.setBounds(804, 166, 105, 49);
+		// 쓰레드라벨
 		JLabel lbTime4 = new JLabel("");
-		lbTime4.setBounds(410, 82, 105, 49);
+		lbTime4.setBounds(836, 122, 105, 49);
 		jp.add(lbTime4);
-		CallThread(lbTime4,roomInfoList.get(3).getRemTime(),roomInfoList,3,btnRoom4);
-		
+		CallThread(lbTime4, roomInfoList.get(3).getRemTime(), roomInfoList, 3, btnRoom4);
+
 		JButton btnRoom5 = new JButton("5\uBC88\uBC29");
-		btnRoom5.setBounds(14, 288, 105, 49);
-		//쓰레드라벨
+		btnRoom5.setBounds(88, 355, 105, 49);
+		// 쓰레드라벨
 		JLabel lbTime5 = new JLabel("");
-		lbTime5.setBounds(50, 250, 105, 49);
+		lbTime5.setBounds(120, 312, 105, 49);
 		jp.add(lbTime5);
-		CallThread(lbTime5,roomInfoList.get(4).getRemTime(),roomInfoList,4,btnRoom5);
-	
+		CallThread(lbTime5, roomInfoList.get(4).getRemTime(), roomInfoList, 4, btnRoom5);
+
 		JButton btnRoom6 = new JButton("6\uBC88\uBC29");
-		btnRoom6.setBounds(131, 288, 105, 49);
-		//쓰레드라벨
+		btnRoom6.setBounds(327, 355, 105, 49);
+		// 쓰레드라벨
 		JLabel lbTime6 = new JLabel("");
-		lbTime6.setBounds(180, 250, 105, 49);
+		lbTime6.setBounds(360, 312, 105, 49);
 		jp.add(lbTime6);
-		CallThread(lbTime6,roomInfoList.get(5).getRemTime(),roomInfoList,5,btnRoom6);
-		
+		CallThread(lbTime6, roomInfoList.get(5).getRemTime(), roomInfoList, 5, btnRoom6);
+
 		JButton btnRoom7 = new JButton("7\uBC88\uBC29");
-		btnRoom7.setBounds(250, 288, 105, 49);
-		//쓰레드라벨
+		btnRoom7.setBounds(578, 355, 105, 49);
+		// 쓰레드라벨
 		JLabel lbTime7 = new JLabel("");
-		lbTime7.setBounds(290, 250, 105, 49);
+		lbTime7.setBounds(610, 312, 105, 49);
 		jp.add(lbTime7);
-		CallThread(lbTime7,roomInfoList.get(6).getRemTime(),roomInfoList,6,btnRoom7);
-		
+		CallThread(lbTime7, roomInfoList.get(6).getRemTime(), roomInfoList, 6, btnRoom7);
+
 		JButton btnRoom8 = new JButton("8\uBC88\uBC29");
-		btnRoom8.setBounds(369, 288, 105, 49);	
-		//쓰레드라벨
+		btnRoom8.setBounds(804, 355, 105, 49);
+		// 쓰레드라벨
 		JLabel lbTime8 = new JLabel("");
-		lbTime8.setBounds(400, 250, 105, 49);
+		lbTime8.setBounds(836, 312, 105, 49);
 		jp.add(lbTime8);
-		CallThread(lbTime8,roomInfoList.get(7).getRemTime(),roomInfoList,7,btnRoom8);
-		
-		
-		//투명??
-		//btnRoom8.setBackground(SystemColor.textHighlightText);
-		
-		
+		CallThread(lbTime8, roomInfoList.get(7).getRemTime(), roomInfoList, 7, btnRoom8);
+
+		// 투명??
+		// btnRoom8.setBackground(SystemColor.textHighlightText);
+
 		jp.add(btnRoom1);
 		jp.add(btnRoom2);
 		jp.add(btnRoom3);
@@ -106,13 +102,12 @@ public class MainRoomView extends SpaceActionEvent implements ViewIndex{
 		jp.add(btnRoom6);
 		jp.add(btnRoom7);
 		jp.add(btnRoom8);
-		
+
 		JButton btnAdmin = new JButton("\uAD00\uB9AC\uC790");
-		
-		btnAdmin.setBounds(398, 446, 105, 27);
+
+		btnAdmin.setBounds(27, 626, 105, 27);
 		jp.add(btnAdmin); // 관리자버튼
 
-		
 		btnRoom1.addMouseListener(new SpaceActionEvent(MAIN_ROOM_VIEW_NUM, 1, sc, mainJframe, jp));
 		btnRoom2.addMouseListener(new SpaceActionEvent(MAIN_ROOM_VIEW_NUM, 2, sc, mainJframe, jp));
 		btnRoom3.addMouseListener(new SpaceActionEvent(MAIN_ROOM_VIEW_NUM, 3, sc, mainJframe, jp));
@@ -122,26 +117,20 @@ public class MainRoomView extends SpaceActionEvent implements ViewIndex{
 		btnRoom7.addMouseListener(new SpaceActionEvent(MAIN_ROOM_VIEW_NUM, 7, sc, mainJframe, jp));
 		btnRoom8.addMouseListener(new SpaceActionEvent(MAIN_ROOM_VIEW_NUM, 8, sc, mainJframe, jp));
 		btnAdmin.addMouseListener(new SpaceActionEvent(MAIN_ROOM_VIEW_NUM, 9, sc, mainJframe, jp));
-		
-		
+
 		mainJframe.add(jp);
 		jp.revalidate();
 		jp.repaint();
-		
-		
-		
-		//jp.setVisible(true);
-		//mainJframe.setVisible(true);
+
+		// jp.setVisible(true);
+		// mainJframe.setVisible(true);
 	}
-	
-	
-/////쓰레드////////	
-	public void CallThread(JLabel lbl,int remTime,List<RoomInfo> list,int index,JButton btn)
-	{
-		TimerThread tt = new TimerThread(lbl,remTime,list,index,btn);
-		
+
+/////쓰레드////////   
+	public void CallThread(JLabel lbl, int remTime, List<RoomInfo> list, int index, JButton btn) {
+		TimerThread tt = new TimerThread(lbl, remTime, list, index, btn);
+
 		tt.start();
 	}
-	
 
 }
--- src/com/space_distortion/view/SubRoomView.java
+++ src/com/space_distortion/view/SubRoomView.java
@@ -11,109 +11,93 @@ import com.space_distortion.controller.SpaceController;
 import com.space_distortion.event.SpaceActionEvent;
 import com.space_distortion.model.vo.RoomInfo;
 
-public class SubRoomView extends SpaceActionEvent implements ViewIndex{
+public class SubRoomView extends SpaceActionEvent implements ViewIndex {
 
-	
-	//private Main win;
-	//private SpaceController sc = new SpaceController();
+	// private Main win;
+	// private SpaceController sc = new SpaceController();
 	private JLabel lb1; // 방이름
 	private JLabel lb2; // 방번호
-	private JLabel lb3; // 수용인원 
+	private JLabel lb3; // 수용인원
 	private JLabel lb4; // 빔프로젝트
 	private JLabel lb5;
-//	private JLabel lb6; // 노트북 알림
-//	private JLabel lb7;
+//   private JLabel lb6; // 노트북 알림
+//   private JLabel lb7;
 	private JButton btnBack;
 	private JButton btnMember;
 	private JButton btnNoMember;
 	private JButton btnReservation;
-	
-	
-	
-	
+
 	public SubRoomView() {
-		
+
 	}
-	
-	public void initialize(SpaceController sc, JFrame mainJframe,List roomInfoList, int roomNumber)
-	{
-		
+
+	public void initialize(SpaceController sc, JFrame mainJframe, List roomInfoList, int roomNumber) {
+
 		JPanel jp = new JPanel();
 		jp.setBounds(10, 10, 600, 600);
 		jp.setLayout(null);
-		
-		
-		
+
 		lb1 = new JLabel("");
 		lb1.setBounds(14, 12, 242, 52);
 		jp.add(lb1);
-			
+
 		lb2 = new JLabel("");
 		lb2.setBounds(14, 105, 172, 52);
 		jp.add(lb2);
-		
+
 		lb3 = new JLabel("");
 		lb3.setBounds(14, 171, 172, 52);
 		jp.add(lb3);
-		
-		
+
 		lb4 = new JLabel("");
 		lb4.setBounds(10, 306, 230, 27);
 		jp.add(lb4);
-		
-	
+
 		btnBack = new JButton("처음 화면으로");
-		btnBack.setBounds(331, 25, 138, 27);
+		btnBack.setBounds(840, 23, 138, 27);
 		jp.add(btnBack);
-		
 
 		btnMember = new JButton("회원");
-		btnMember.setBounds(35, 414, 105, 27);
+		btnMember.setBounds(99, 585, 121, 58);
 		jp.add(btnMember);
-		
+
 		btnNoMember = new JButton("비회원");
-		btnNoMember.setBounds(198, 414, 105, 27);
+		btnNoMember.setBounds(427, 585, 121, 58);
 		jp.add(btnNoMember);
-		
+
 		btnReservation = new JButton("예약");
-		btnReservation.setBounds(361, 414, 105, 27);
+		btnReservation.setBounds(779, 585, 121, 58);
 		jp.add(btnReservation);
-		
-		lb1.setText(   ((RoomInfo)roomInfoList.get(roomNumber-1)).getRoomName());
-		lb2.setText("방번호 :  " + String.valueOf(   ((RoomInfo)roomInfoList.get(roomNumber-1)).getRoomNumber())); //스트링으러 바꿈
-		lb3.setText("수용인원 :  " + String.valueOf(((RoomInfo)roomInfoList.get(roomNumber-1)).getMaxPpl()  ));
-		lb4.setText(((RoomInfo)roomInfoList.get(roomNumber-1)).getLightProjecter());
-		
+
+		lb1.setText(((RoomInfo) roomInfoList.get(roomNumber - 1)).getRoomName());
+		lb2.setText("방번호 :  " + String.valueOf(((RoomInfo) roomInfoList.get(roomNumber - 1)).getRoomNumber())); // 스트링으러
+																												// 바꿈
+		lb3.setText("수용인원 :  " + String.valueOf(((RoomInfo) roomInfoList.get(roomNumber - 1)).getMaxPpl()));
+		lb4.setText(((RoomInfo) roomInfoList.get(roomNumber - 1)).getLightProjecter());
+
 		JLabel lbImage = new JLabel("\uBC29\uAD6C\uC870 \uC774\uBBF8\uC9C0");
 		lbImage.setBounds(288, 171, 172, 52);
 		jp.add(lbImage);
-		
-//		lb4.setText("수용인원 :  " + String.valueOf(sc.roomInfoList.get(num-1).getCurPpl()));
-//		lb6.setText((sc.roomInfoList.get(num-1).getNoteBook()));
 
-		
-		btnMember.addMouseListener(new SpaceActionEvent(SUB_ROOM_VIEW_NUM, 1, sc, mainJframe ,jp)); // 회원
-		btnNoMember.addMouseListener(new SpaceActionEvent(SUB_ROOM_VIEW_NUM, 2, sc, mainJframe ,jp)); // 비회원
-		btnReservation.addMouseListener(new SpaceActionEvent(SUB_ROOM_VIEW_NUM, 3, sc, mainJframe ,jp));// 예약
-		btnBack.addMouseListener(new SpaceActionEvent(SUB_ROOM_VIEW_NUM, 4, sc, mainJframe ,jp)); // 뒤로가기
-		
-		
-		
+//      lb4.setText("수용인원 :  " + String.valueOf(sc.roomInfoList.get(num-1).getCurPpl()));
+//      lb6.setText((sc.roomInfoList.get(num-1).getNoteBook()));
+
+		btnMember.addMouseListener(new SpaceActionEvent(SUB_ROOM_VIEW_NUM, 1, sc, mainJframe, jp)); // 회원
+		btnNoMember.addMouseListener(new SpaceActionEvent(SUB_ROOM_VIEW_NUM, 2, sc, mainJframe, jp)); // 비회원
+		btnReservation.addMouseListener(new SpaceActionEvent(SUB_ROOM_VIEW_NUM, 3, sc, mainJframe, jp));// 예약
+		btnBack.addMouseListener(new SpaceActionEvent(SUB_ROOM_VIEW_NUM, 4, sc, mainJframe, jp)); // 뒤로가기
+
 		mainJframe.add(jp);
-		
-		
-		
+
 		jp.revalidate();
 		jp.repaint();
-		
-		
-		
-		//mainJframe.setContentPane(jp);
-		//jp.setVisible(true);
-		
-//		jp.revalidate();
-//		jp.repaint();
-//		mainJframe.setVisible(true);
-	
+
+		// mainJframe.setContentPane(jp);
+		// jp.setVisible(true);
+
+//      jp.revalidate();
+//      jp.repaint();
+//      mainJframe.setVisible(true);
+
 	}
 }
